33c3e2da4ed235810d22fd83b4c7f8face2a658c
Susumu Yamazaki <zacky1972@gmail.com>
2022-07-26 16:26:01 +0900

    initial commit

---
+++ lib/ex_metal_sample.ex
@@ -0,0 +1,18 @@
+defmodule ExMetalSample do
+  @moduledoc """
+  Documentation for `ExMetalSample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExMetalSample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_metal_sample_test.exs
@@ -0,0 +1,8 @@
+defmodule ExMetalSampleTest do
+  use ExUnit.Case
+  doctest ExMetalSample
+
+  test "greets the world" do
+    assert ExMetalSample.hello() == :world
+  end
+end
