659b62d3e89cbd7360ebfcc62d303263fcba42b4
Susumu Yamazaki <zacky1972@gmail.com>
2022-07-27 14:03:21 +0900

    exclude Programming by Coincidence

--- c_src/MetalAdder.h
+++ c_src/MetalAdder.h
@@ -7,7 +7,7 @@ NSString *libraryFile;
 
 @interface MetalAdder : NSObject 
 - (instancetype) initWithDevice: (id<MTLDevice>) device error:(char*)error;
-- (void) prepareData: (const int32_t*)inA inB: (const int32_t*)inB size: (size_t)vec_size error:(char*)error;
+- (bool) prepareData: (const int32_t*)inA inB: (const int32_t*)inB size: (size_t)vec_size error:(char*)error;
 - (int32_t*) sendComputeCommand: (size_t)vec_size error:(char*)error;
 @end
 
