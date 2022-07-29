06b34d254d37e9349c79e76756ef4cc6c11ecc73
Susumu Yamazaki <zacky1972@gmail.com>
2022-07-30 04:13:56 +0900

    fix ret_error length

--- c_src/libnif.c
+++ c_src/libnif.c
@@ -65,7 +65,7 @@ static ERL_NIF_TERM add_s32_nif(ErlNifEnv *env, int argc, const ERL_NIF_TERM arg
     memset(error, 0, MAXBUFLEN);
 
     if(__builtin_expect(!add_s32_metal(in1, in2, out, vec_size, error), false)) {
-        char ret_error[MAXBUFLEN];
+        char ret_error[MAXBUFLEN + strlen(metal_error)];
         snprintf(ret_error, MAXBUFLEN + strlen(metal_error), "%s%s", metal_error, error);
         return enif_raise_exception(env, enif_make_string(env, ret_error, ERL_NIF_LATIN1));
     }
